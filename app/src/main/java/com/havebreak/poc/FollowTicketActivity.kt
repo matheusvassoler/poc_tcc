172bdc91652a88c20d58bf2bef0d6b3274787c0e
Matheus Vassoler <matheus.s.vassoler@gmail.com>
2020-11-10 23:08:22 -0300

    First commit

---
+++ app/src/main/java/com/havebreak/poc/FollowTicketActivity.kt
@@ -0,0 +1,11 @@
+package com.havebreak.poc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FollowTicketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follow_ticket)
+    }
+}
---
+++ app/src/main/java/com/havebreak/poc/OpenTicketActivity.kt
@@ -0,0 +1,11 @@
+package com.havebreak.poc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenTicketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_ticket)
+    }
+}
